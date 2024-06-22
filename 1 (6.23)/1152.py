81d622a485b7507aa6d39af28def92ab39b4a1c9
nanaring1 <166397141+nanaring1@users.noreply.github.com>
2024-06-22 15:53:26 +0900

    1152제출

---
+++ 1 (6.23)/1152.py
@@ -0,0 +1,11 @@
+string = "The Curious Case of Benjamin Button"
+words = string.split()
+print(len(words))
+
+string="The first character is a blank"
+words = string.split()
+print(len(words))
+
+string="The last character is a blank"
+words=string.split()
+print(len(words))
